76ea098d31171dba2e0faab0af21de4aab1263b8
yangxiaohu <yangxiaohu2015@163.com>
2025-01-24 14:14:50 +0800

    feat:section 6

--- src/components/section-6-pyramid/index.tsx
+++ src/components/section-6-pyramid/index.tsx
@@ -1,23 +1,29 @@
 import PlusIcon from '@/assets/icons/plus-icon.svg?react'
 import Pyramid from '@/assets/triangle.svg?react'
 
+import image1 from '@/assets/pyramid-image-1.png?url'
+import image2 from '@/assets/pyramid-image-2.png?url'
+import image3 from '@/assets/pyramid-image-3.png?url'
+
 type TItemProps = {
   text: string | JSX.Element
   image: string
+  className?: string
 }
 
 const data = [
   {
     text: (
-      <>
+      <div className="mb-1">
         Real-world <br />
         Robot Data
-      </>
+      </div>
     ),
-    image: ''
+    image: image1,
+    className: 'w-[calc(235/640*100%)]'
   },
-  { text: 'Simulation Data', image: '' },
-  { text: 'Internet Data', image: '' }
+  { text: <div className="pb-3">Simulation Data</div>, image: image2, className: 'w-[calc(430/640*100%)]' },
+  { text: <div className="pb-7">Internet Data</div>, image: image3, className: 'w-[calc(640/640*100%)]' }
 ]
 
 export default function Section() {
@@ -25,8 +31,8 @@ export default function Section() {
     <div className="section flex flex-col items-center py-10 text-center">
       <h2 className="mb-16 text-3xl font-bold text-primary-light">Data Pyramid</h2>
       <PlusIcon className="mt-20 size-8 text-primary-dark" />
-      <div className="relative z-0 mt-[70px] flex aspect-[640/560] w-full flex-col pt-14 text-sm">
-        <Pyramid className="absolute top-0 -z-10 mx-auto size-full" width={640} height={560} />
+      <div className="relative z-0 my-3 mt-[70px] flex aspect-[640/560] w-full flex-col items-center text-sm">
+        <Pyramid className="pointer-events-none absolute top-0 z-20 mx-auto size-full" width={640} height={560} />
         {data.map((item, index) => (
           <Item key={item.image + index} {...item} />
         ))}
@@ -35,10 +41,15 @@ export default function Section() {
   )
 }
 
-function Item({ text }: TItemProps) {
+function Item({ text, image, className }: TItemProps) {
   return (
-    <div className="flex flex-1 items-center justify-center">
-      <span>{text}</span>
+    <div className={`group relative flex flex-1 items-center justify-center ${className}`}>
+      <div className="absolute flex size-full items-end justify-center text-sm">{text}</div>
+      <img
+        src={image}
+        alt=""
+        className="relative z-10 h-full w-auto opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+      />
     </div>
   )
 }
