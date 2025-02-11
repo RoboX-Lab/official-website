4390c65ddc4c1f3c8db74acaf4ea969b9a60d80b
yangxiaohu <yangxiaohu2015@163.com>
2025-02-11 21:06:09 +0800

    feat:update twitter link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -44,7 +44,7 @@ export default function Footer() {
 }
 
 const links = [
-  { name: 'twitter', href: 'https://x.com/Roboagent69', icon: <TwitterIcon className="size-6 md:size-9" /> },
+  { name: 'twitter', href: 't.me/Roboagent69Channel', icon: <TwitterIcon className="size-6 md:size-9" /> },
   { name: 'idLNtyBAm', href: '#', icon: <IdLNtyBAmIcon className="size-6 md:size-9" /> },
   { name: 'note', href: '#', icon: <NoteIcon className="size-6 md:size-9" /> }
 ]
