e38ef6f5bfb801b26f1a240ceccfc1361f44d956
yangxiaohu <yangxiaohu2015@163.com>
2025-02-13 23:31:11 +0800

    feat:ca

--- src/components/section-1-home/index.tsx
+++ src/components/section-1-home/index.tsx
@@ -25,9 +25,9 @@ function Circle() {
     <div className="relative z-0 flex shrink-0 flex-col items-center justify-center">
       <Logo className="mx-auto my-10 block md:h-[200px] md:w-[160px]" borderColor="#FF6501" />
       <div className="mx-auto leading-6">Your Agentic Copilot, from Virtual to Real</div>
-      {/* <div className="mt-6 w-full rounded-md bg-[#FFFFFF1F] px-3 py-2 text-center text-sm leading-6">
-        CA: dasfaerwdasfaer1232dfawadf24sadfasdf
-      </div> */}
+      <div className="mt-6 w-full rounded-md bg-[#FFFFFF1F] px-3 py-2 text-center text-sm leading-6">
+        CA: CzbsTtLTwnX81fEvNBz7bDSt8VAw43XAcdgK8Ludvirt
+      </div>
       <div className="absolute left-1/2 top-1/2 -z-10 aspect-1 size-[400px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-gradient-radial from-[#241003] to-[black] md:size-[560px]"></div>
     </div>
   )
